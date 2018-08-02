2ccab9ff35a2ff695392e4745bdafe910e5b4f07
Abanoub Ghadban <ghdbana@gmail.com>
2018-08-02 17:38:59 +0200

    Added route to add list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from Blockchain import Blockchain
 
 chain = Blockchain()
@@ -8,6 +9,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return chain.toJson()
+
+
+@app.route('/transactions/addList', methods=['POST'])
+def addList():
+    jsonTs = request.get_json().get('transactions')
+    chain.addTransactionsFromJsonList(jsonTs)
+    return str(len(jsonTs)) + " Transaction(s) Added"
 
 
 app.run()
