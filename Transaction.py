0dca3d2441ec7f6f3afd50073e12c33af380b2d2
Abanoub Ghadban <ghdbana@gmail.com>
2018-08-02 15:03:04 +0200

    Added Transaction.py

---
+++ Transaction.py
@@ -0,0 +1,8 @@
+class Transaction:
+    def __init__(self, sender, receiver, amount):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+
+    def toString(self):
+        return str(self.sender) + " --> " + str(self.receiver) + " (" + str(self.amount) + ")"
