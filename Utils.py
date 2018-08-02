9adbaa3c48c84b0f2d8af979ebab6182e218c700
Abanoub Ghadban <ghdbana@gmail.com>
2018-08-02 17:41:30 +0200

    Added function to parse json in Utils

--- Utils.py
+++ Utils.py
@@ -1,7 +1,12 @@
 import hashlib
+import jsonpickle
 
 
 def sha256(data):
     hash_object = hashlib.sha256(data)
     hex_dig = hash_object.hexdigest()
     return hex_dig
+
+
+def parseFromJson(strData):
+    return jsonpickle.decode(strData)
