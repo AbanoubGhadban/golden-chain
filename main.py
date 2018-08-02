09b3f2226581b2a52647c026e5b6a2db34889832
Abanoub Ghadban <ghdbana@gmail.com>
2018-08-02 16:04:05 +0200

    Added index route to main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from Blockchain import Blockchain
 
+chain = Blockchain()
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return chain.toJson()
+
 
 app.run()
