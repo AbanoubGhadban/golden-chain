c8b85d6101c4d35fc1d4d63252741c4f46f3f465
Abanoub Ghadban <ghdbana@gmail.com>
2018-08-02 17:32:06 +0200

    Adding function to parse Json to Transaction

--- Transaction.py
+++ Transaction.py
@@ -4,5 +4,12 @@ class Transaction:
         self.receiver = receiver
         self.amount = amount
 
+    @staticmethod
+    def parseFromJsonObject(jsonTr):
+        sender = jsonTr.get("sender")
+        receiver = jsonTr.get("receiver")
+        amount = jsonTr.get("amount")
+        return Transaction(sender, receiver, amount)
+
     def toString(self):
         return str(self.sender) + " --> " + str(self.receiver) + " (" + str(self.amount) + ")"
